4abd4ffb5dcba26c80462e95502d38830eeab578
nika518 <nikoloz.gelashvili.01@gmail.com>
2025-03-03 19:30:24 +0400

    first commit

---
+++ day 1/level01/homework/nikoloz.py
@@ -0,0 +1,23 @@
+name = "my name is nika"
+
+#welcome = "Hello"
+
+surename = "gelashvili"
+
+born = "I was born city khashuri"
+
+knowladge = "I finished Tbilisi state university"
+
+print(name + " " + surename)
+
+age = 24
+
+heigh = 179.9
+
+print(name + " " + str(age)  + " " + born + " " + knowladge + " " + str(heigh))
+
+
+print(name + str(heigh))
+
+
+print(type(heigh))

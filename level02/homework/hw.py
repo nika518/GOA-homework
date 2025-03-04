6b3cd7c7c96f12ef2c013ef1368855d49bb9294b
nika518 <nikoloz.gelashvili.01@gmail.com>
2025-03-04 20:58:13 +0400

    commit

---
+++ level02/homework/hw.py
@@ -0,0 +1,12 @@
+print("hello world")
+print(15)
+a = "გამარჯობა"
+print(a)
+x = "ელემენტი"
+print("ელემენტი" * 10)
+print(10 * 100)
+a = 'hello'
+b = 'world'
+print(a + ' ' + b)
+print(10 > 5)
+print(10 < 9)

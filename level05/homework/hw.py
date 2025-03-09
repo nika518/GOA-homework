5ac043e733c40212ec9b966c75266fb376dc7c74
nika518 <nikoloz.gelashvili.01@gmail.com>
2025-03-09 22:43:51 +0400

    commit

--- level05/homework/hw.py
+++ level05/homework/hw.py
@@ -1,34 +1,34 @@
 #1) for ციკლის გამოყენებით გამოიტანეთ რიცხვები 1-დან 10-მდე
 
-for x in range(1,11):
-   print(x)
+for i in range(1,11):
+   print(i)
 
  
 
 #2) for ციკლის გამოყენებით გამოიტანეთ რიცხვები 5-დან 25-მდე
-for x in range(5,26):
-    print(x)
+for i in range(5,26):
+    print(i)
 
 
 
 #3) for ციკლის გამოყენებით გამოიტანეთ რიცხვები 10-დან 100-მდე, 5ის stepით
 
-for x in range(10,15,20,101):
-    print(x)
+for i in range(10,101,5):
+    print(i)
+
 
 
 
 #4) მომხმარებელს შემოატანინეთ ნებისმიერი სიტყვა, შემდეგ ამ სიტყვიდან გამოიტანეთ მხოლოდ 'A' ასოები, თუ არ შეიცავს 'A'ს გამოიტანეთ ცარიელი სტრინგი
 
-name = input("please enter your name")
-amount = 0
-for i in name:
-    if i == 'A':
+word = input('enter your name') #manana
+for i in word:
+    if i == "A":
         print(i)
-        amount - amount + 1
-if amount == 0:
-    print("there are not any A letter")        
-
+else:
+    print("There is no A")        
 
 
 #5) კომენტარებით დაწერეთ რას გამოიტანს შემდეგი გამოსახულება: True or True and False or True and False and False and True or False
+
+print(True or True and False or True and False and False and True or False)

8cfab085d4e6ee740bc98d791cfc94bc73d1c299
nika518 <nikoloz.gelashvili.01@gmail.com>
2025-03-29 21:24:43 +0400

    commit

--- level09/homework/hw.py
+++ level09/homework/hw.py
@@ -1,15 +1,16 @@
-'''def repeat_str(repeat, string):
+#1
+def repeat_str(repeat, string):
     return repeat * string
 
 
 
-
+#2
 def say_hello(name):
     return "Hello,"+" "+ name
 
 
 
-
+#3
 def rps(p1, p2):
     if p1==p2:
         return "Draw!"
@@ -23,7 +24,7 @@ def rps(p1, p2):
         return "Player 2 won!"
     
 
-
+#4
 def get_grade(s1, s2, s3):
     avarage = (s1+s2+s3)/3
     if 90<=avarage<=100:
@@ -38,13 +39,13 @@ def get_grade(s1, s2, s3):
         return 'F'
     
 
-
+#5
 def simple_multiplication(number) :
     if number%2 == 0:
         return number * 8
     else:
         return number * 9
-    '''
+
 
 
 #  შექმენით ფუნქცია რომელსაც გადაეცემა ორი პარამეტრი name, lastname. თქვენი დავალებაა გამოიტანოთ სახელის პირველი ასო, წერტილი და გვარი, მაგალითად:
